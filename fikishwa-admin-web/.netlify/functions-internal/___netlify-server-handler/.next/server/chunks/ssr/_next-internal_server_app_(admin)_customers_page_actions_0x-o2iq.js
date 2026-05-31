37e94e6ae7a12eae4b1be7cd0d63ca1434c09fae
hiuhuedd <hiuhuedd@gmail.com>
2026-05-31 22:39:26 +0300

    feat(admin-web): update dashboard UI and netlify plugins

---
+++ fikishwa-admin-web/.netlify/functions-internal/___netlify-server-handler/.next/server/chunks/ssr/_next-internal_server_app_(admin)_customers_page_actions_0x-o2iq.js
@@ -0,0 +1,3 @@
+module.exports=[12468,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_%28admin%29_customers_page_actions_0x-o2iq.js.map
---
+++ fikishwa-admin-web/.netlify/functions-internal/___netlify-server-handler/.next/server/chunks/ssr/_next-internal_server_app_(admin)_drivers_page_actions_0sqyb-1.js
@@ -0,0 +1,3 @@
+module.exports=[85688,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_%28admin%29_drivers_page_actions_0sqyb-1.js.map
